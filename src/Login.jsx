71f135eeb2363c758984174aab98995e72160ea9
unknown <uwohchizoba@gmail.com>
2023-01-29 18:36:19 +0100

    stan's fifth commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Login = (props) => {
-const[email, setEmail] = useState ('');
+const [matric, setMatric] = useState('');
 const[pass, setPass] = useState('');
 
 const handleSubmit = (e) =>  {
     e.preventDefault();
-    console.log(email);
+    console.log(matric);
 
 }
 
@@ -15,12 +15,12 @@ const handleSubmit = (e) =>  {
         <div className="auth-form-container">
             <h2>Student Login</h2>
             <form className="login-form"onSubmit= {handleSubmit}>
-                <label htmlFor="email"> Email </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder= "youremail@gmail.com" id="email" name="email"/>
+                <label htmlFor="matric"> Matric Number </label>
+                <input value={matric} onChange={(e) => setMatric(e.target.value)} type="matric" id="matric" placeholder="matric number" matric="matric" />
             
                 <label htmlFor="password"> Password </label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder= "********" id="password" name="password"/>
-                <Link to="/Pages/Home"><button type="submit">Log In</button></Link>
+                <Link to="/"><button type="submit">Log In</button></Link>
             </form>
             <Link to="/Register"><button className="link-btn">Don't have an account? Register here.</button></Link>
          </div>
